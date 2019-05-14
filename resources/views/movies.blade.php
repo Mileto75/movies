84e71fedcfe4604fbb76268bec4230c65662c8cb
mileto1975 <mileto.di.marco@howest.be>
2019-05-14 17:52:01 +0200

    added view to edit movie

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,6 +1,23 @@
-@include('layout/header')
+@extends('layouts.app')
 
+@section('content')
+    <table class="table table-responsive">
+        <thead class="thead-dark">
+        <tr>
+            <th>Titel</th>
+            <th>Jaar</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($films as $film)
+            <tr>
+                <td>{{$film->titel}}</td>
+                <td>{{$film->jaar}}</td>
+                <td><a href="{{route("editMovie",['filmId' => $film->film_id])}}">Edit</a> </td>
+            </tr>
+        @endforeach
+        </tbody>
 
-{{$films->titel}}
-
-@include('layout/footer');
+    </table>
+@endsection
