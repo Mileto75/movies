51bd37280175a071ec73b31e937c02acb5655238
mileto1975 <mileto.di.marco@howest.be>
2019-05-15 15:24:25 +0200

    added DB facade select method

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -4,23 +4,52 @@ namespace App\Http\Controllers;
 
 use App\films;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class MoviesController extends Controller
 {
     //
 
-    public function index()
+    public function __construct()
     {
-
+        $this->middleware('auth');
     }
 
     public function toonFilms()
     {
-        $film = films::find(1);
-        $film->titel = 'The Godfather 2';
-        //var_dump($film);
+        //using model
+        //$film = films::find(1);
 
-        $film->save();
-        return view('movies',['films' =>$film]);
+        //using DB facade
+        $films = DB::select('select * from tbl_films');
+        return view('movies',['films' =>$films]);
+    }
+
+    /**
+     * @param $filmId
+     * Haalt info op van film
+     * toont de editMovie view
+     */
+    public function editMovie($filmId)
+    {
+        //leg de parameters vast om mee te geven
+        $ar_params = array('film_id' => $filmId);
+        //bouw de query op met placeholder :film_id
+        $filmData = DB::select("SELECT * FROM tbl_films WHERE film_id=:film_id",$ar_params);
+        //de DB::select methode retourneert een array vanb objecten
+        //het resultaat kunnen we dan dmv de $vars meegeven aan de view
+        $vars = ['filmId' => $filmId,'filmData' => $filmData];
+        return view('editMovie',$vars);
+
+    }
+
+    /**
+     * update de movie in de db
+     * redirect naar movies view met
+     * bericht
+     */
+    public function updateMovie(Request $request)
+    {
+        $ar_rules = array('titel' => 'required','jaar'=>'required|integer|between:1888,2020');
+        $request->validate($ar_rules);
     }
 }
